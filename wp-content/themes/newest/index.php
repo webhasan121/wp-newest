0fa59edaa3ea3c65bb659bb45e02e43d16c82b86
imtiaz6436@gmail.com <imtiaz6436@gmail.com>
2021-04-25 19:45:35 +0600

    ready ois

--- wp-content/themes/newest/index.php
+++ wp-content/themes/newest/index.php
@@ -9,7 +9,7 @@
                                 <h2>Blog Page</h2>
                                 <ul class="breadcrumb-menu">
                                     <li class="breadcrumb-items"><a href="index.html" title="Home">Home</a></li>
-                                    <li class="active">Blog date_formatssss sdfsa dsfads </li>
+                                    <li class="active">Blog</li>
                                 </ul>
                             </div>
                         </div>
